d9f11fbd077b5ae9253d435c04b6ed660300265f
muki wu <mukispace@gmail.com>
2020-11-04 16:24:30 +0800

    新增首頁訂閱

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 
+// Line Notify
+const clientId = process.env.LINE_NOTIFY_CLIENT_ID;
+const clientSecret = process.env.LINE_NOTIFY_CLIENT_SECRET;
+const redirectUri = `${process.env.ROOT_PATH}/callback`;
+
+let URL = 'https://notify-bot.line.me/oauth/authorize?';
+    URL += 'response_type=code';
+    URL += '&client_id='+clientId;
+    URL += '&redirect_uri='+redirectUri;
+    URL += '&scope=notify';
+    URL += '&state=astro';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    url: URL
+  })
 });
 
 module.exports = router;
