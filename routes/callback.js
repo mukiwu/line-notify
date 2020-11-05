30bebc5aa63cb4a1938d0b796be5b76b7f20407a
muki wu <mukispace@gmail.com>
2020-11-05 17:06:31 +0800

    將 line notify 用到的 API 單獨拆出來

--- routes/callback.js
+++ routes/callback.js
@@ -1,24 +1,53 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
+const lineNotify = require('../src/lineNotify');
 
 // Line Notify
 const clientId = process.env.LINE_NOTIFY_CLIENT_ID;
 const clientSecret = process.env.LINE_NOTIFY_CLIENT_SECRET;
 const redirectUri = `${process.env.ROOT_PATH}/callback`;
+const subscriptions = [];
 
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res) {
+  // const formData = {
+  //   grant_type: 'authorization_code',
+  //   code: req.query.code,
+  //   redirect_uri: redirectUri,
+  //   client_id: clientId,
+  //   client_secret: clientSecret,
+  // }
+  // const headers = {
+  //   'Content-Type': 'application/x-www-form-urlencoded',
+  // };
+
+  // axios.post('https://notify-bot.line.me/oauth/token', querystring.encode(formData), {headers})
+  // .then(res => {
+  //   subscriptions.push(res.data.access_token)
+  //   return subscriptions
+  // })
+  // .catch(error => {
+  //   console.log(error)
+  // })
+  // console.log('subscriptions', subscriptions)
+
   const code = req.query.code;
-  res.render('callback', {
-    url: URL
-  })
-  console.log('code', code)
+  const response = await lineNotify.getToken(code, redirectUri, clientId, clientSecret);
+  const token = response.data.access_token;
+  subscriptions.push(token);
+  await lineNotify.sendNotify(token, "恭喜完成訂閱！");
+  console.log('token', token)
+  console.log('subscriptions', subscriptions)
+  res.render('callback');
+
+  // res.render('callback', {
+  //   token: subscriptions
+  // })
 });
 
 module.exports = router;
